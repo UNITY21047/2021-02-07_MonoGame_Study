c14225f0c17c151fbcd4d40a3f783819eb1bfee8
Gary Smith <werty21047@gmail.com>
2021-02-07 10:42:09 -0600

    Major Update: Added test assets (box.png etc.); added assets to the content major.

--- pt_1/Game1.cs
+++ pt_1/Game1.cs
@@ -8,6 +8,8 @@ namespace pt_1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D box;
+        private Vector2 box_position;
 
         public Game1()
         {
@@ -19,33 +21,73 @@ namespace pt_1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            box_position = new Vector2(100f);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            box = Content.Load<Texture2D>("box");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
-
+            }
+            
             // TODO: Add your update logic here
+            #region Keyboard
+            // Allows the game to exit
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                this.Exit();
+            }
+
+            // Update the image positions with AWSD
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                box_position.X--;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                box_position.X++;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                box_position.Y--;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                box_position.Y++;
+            }
+            #endregion
+
+            #region Mouse
+            // Poll mouse state
+            MouseState mouse_state = Mouse.GetState();
+            // If left mouse button is pressed
+            if (mouse_state.LeftButton == ButtonState.Pressed)
+            {
+                box_position = new Vector2(mouse_state.X, mouse_state.Y);
+            }
+            #endregion
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.FromNonPremultiplied(187, 68, 48, 100));
 
             // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(box, box_position, Color.White);
+            _spriteBatch.End();
+            
             base.Draw(gameTime);
         }
     }
